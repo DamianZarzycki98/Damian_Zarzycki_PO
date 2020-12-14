d34d6fd9163cf8259eb926d59fb5745b116e810e
Damian <damian.zarzycki98@gmail.com>
2020-12-15 00:52:47 +0100

    Testowe uruchomienie programu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) { launch(args); }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("/FXML/main.fxml"));
+        AnchorPane anchorPane = fxmlLoader.load();
+        Scene scene = new Scene(anchorPane);
+        stage.setScene(scene);
+        stage.setTitle("Biblioteka");
+        stage.show();
     }
 }
--- src/main/java/cfg/MainController.java
+++ src/main/java/cfg/MainController.java
@@ -1,4 +1,43 @@
 package cfg;
 
+import javafx.fxml.*;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+
 public class MainController {
+    @FXML
+    private Pane wyswietl;
+
+    public void autorTable(javafx.event.ActionEvent actionEvent) throws IOException {
+        AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/FXML/autor.fxml"));
+        wyswietl.getChildren().setAll(anchorPane);
+    }
+
+    public void czytTable(javafx.event.ActionEvent actionEvent) throws IOException {
+        AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/FXML/czytelnik.fxml"));
+        wyswietl.getChildren().setAll(anchorPane);
+    }
+
+    public void egzTable(javafx.event.ActionEvent actionEvent) throws IOException {
+        AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/FXML/egzemplarz.fxml"));
+        wyswietl.getChildren().setAll(anchorPane);
+    }
+
+    public void ksTable(javafx.event.ActionEvent actionEvent) throws IOException {
+        AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/FXML/ksiazka.fxml"));
+        wyswietl.getChildren().setAll(anchorPane);
+    }
+
+    public void wydaTable(javafx.event.ActionEvent actionEvent) throws IOException {
+        AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/FXML/wydawnictwo.fxml"));
+        wyswietl.getChildren().setAll(anchorPane);
+    }
+
+    public void wypoTable(javafx.event.ActionEvent actionEvent) throws IOException {
+        AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/FXML/wypozyczenia.fxml"));
+        wyswietl.getChildren().setAll(anchorPane);
+    }
 }
