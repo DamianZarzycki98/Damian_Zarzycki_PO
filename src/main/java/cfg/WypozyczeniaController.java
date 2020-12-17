76e5eb91a703983eb31ae3a64bf6a595a263c736
Damian <damian.zarzycki98@gmail.com>
2020-12-17 19:25:51 +0100

    Wyświetlenie wypozyczeń (początek)

--- src/main/java/cfg/WypozyczeniaController.java
+++ src/main/java/cfg/WypozyczeniaController.java
@@ -1,4 +1,51 @@
 package cfg;
 
-public class WypozyczeniaController {
+import connect.DbConnect;
+import database.Wypozyczenia;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class WypozyczeniaController implements Initializable {
+    @FXML
+    private TableView<Wypozyczenia> wypozyczeniaTab;
+    @FXML
+    private TableColumn<Wypozyczenia, String> wypozyczeniaKsiazka;
+    @FXML
+    private TableColumn<Wypozyczenia, String> wypozyczeniaCzytelnik;
+
+    private Connection connection;
+    private DbConnect dbConnect;
+    private ObservableList<Wypozyczenia> wypozyczeniaList;
+
+    public WypozyczeniaController() {
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        dbConnect = new DbConnect();
+
+        wypozyczeniaTableView();
+    }
+
+    private void wypozyczeniaTableView() {
+        try {
+            wypozyczeniaList = FXCollections.observableArrayList();
+            String query = "SELECT ";
+            connection=dbConnect.getConnection();
+            ResultSet rs = connection.createStatement().executeQuery(query);
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
 }
--- src/main/java/database/Wypozyczenia.java
+++ src/main/java/database/Wypozyczenia.java
@@ -1,4 +1,82 @@
 package database;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 public class Wypozyczenia {
+    @Id
+    @GeneratedValue
+    @Column(name = "Id_Wypozyczenia")
+    private int Id_Wypozyczenia;
+    @Column(name = "Id_Ksiazki")
+    private String Id_Ksiazki;
+    @Column(name = "Nazwa")
+    private String Nazwa;
+    @Column(name = "Id_Czytelnika")
+    private String Id_Czytelnika;
+    @Column(name = "CImNa")
+    private String CImNa;
+
+    public Wypozyczenia() {
+    }
+
+    public Wypozyczenia(int id_Wypozyczenia, String id_Ksiazki, String nazwa, String id_Czytelnika, String CImNa) {
+        Id_Wypozyczenia = id_Wypozyczenia;
+        Id_Ksiazki = id_Ksiazki;
+        Nazwa = nazwa;
+        Id_Czytelnika = id_Czytelnika;
+        this.CImNa = CImNa;
+    }
+
+    public int getId_Wypozyczenia() {
+        return Id_Wypozyczenia;
+    }
+
+    public void setId_Wypozyczenia(int id_Wypozyczenia) {
+        Id_Wypozyczenia = id_Wypozyczenia;
+    }
+
+    public String getId_Ksiazki() {
+        return Id_Ksiazki;
+    }
+
+    public void setId_Ksiazki(String id_Ksiazki) {
+        Id_Ksiazki = id_Ksiazki;
+    }
+
+    public String getNazwa() {
+        return Nazwa;
+    }
+
+    public void setNazwa(String nazwa) {
+        Nazwa = nazwa;
+    }
+
+    public String getId_Czytelnika() {
+        return Id_Czytelnika;
+    }
+
+    public void setId_Czytelnika(String id_Czytelnika) {
+        Id_Czytelnika = id_Czytelnika;
+    }
+
+    public String getCImNa() {
+        return CImNa;
+    }
+
+    public void setCImNa(String CImNa) {
+        this.CImNa = CImNa;
+    }
+
+    @Override
+    public String toString() {
+        return "Wypozyczenia{" +
+                "Id_Wypozyczenia=" + Id_Wypozyczenia +
+                ", Id_Ksiazki='" + Id_Ksiazki + '\'' +
+                ", Nazwa='" + Nazwa + '\'' +
+                ", Id_Czytelnika='" + Id_Czytelnika + '\'' +
+                ", CImNa='" + CImNa + '\'' +
+                '}';
+    }
 }
