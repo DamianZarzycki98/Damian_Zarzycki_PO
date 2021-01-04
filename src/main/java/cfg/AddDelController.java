ea1e9bf50ce5253cbecc4e6d93455b73203107da
Damian <damian.zarzycki98@gmail.com>
2021-01-05 00:30:44 +0100

    Autor dodawanie i usuwanie

--- src/main/java/cfg/AddDelController.java
+++ src/main/java/cfg/AddDelController.java
@@ -1,4 +1,19 @@
 package cfg;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+
 public class AddDelController {
+    @FXML
+    private Pane pane;
+
+    public void autorAddDel(ActionEvent actionEvent) throws IOException {
+        AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/FXML/autorAddDel.fxml"));
+        pane.getChildren().setAll(anchorPane);
+    }
 }
