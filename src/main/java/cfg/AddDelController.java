e690f884c529b33de2798340e7e6dbaed824ab9a
Damian <damian.zarzycki98@gmail.com>
2021-01-05 00:49:20 +0100

    Dodanie FXML oraz Controllerów do dodawani i usuwania

--- src/main/java/cfg/AddDelController.java
+++ src/main/java/cfg/AddDelController.java
@@ -16,4 +16,29 @@ public class AddDelController {
         AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/FXML/autorAddDel.fxml"));
         pane.getChildren().setAll(anchorPane);
     }
+
+    public void czytelnikAddDel(ActionEvent actionEvent) throws IOException{
+        AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/FXML/czytelnikAddDel.fxml"));
+        pane.getChildren().setAll(anchorPane);
+    }
+
+    public void egzemplarzAddDel(ActionEvent actionEvent) throws IOException{
+        AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/FXML/egzemplarzAddDel.fxml"));
+        pane.getChildren().setAll(anchorPane);
+    }
+
+    public void ksiazkaAddDel(ActionEvent actionEvent) throws IOException{
+        AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/FXML/ksiazkaAddDel.fxml"));
+        pane.getChildren().setAll(anchorPane);
+    }
+
+    public void wydawnictwoAddDel(ActionEvent actionEvent) throws IOException{
+        AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/FXML/awydawnictwoAddDel.fxml"));
+        pane.getChildren().setAll(anchorPane);
+    }
+
+    public void wypozyczeniaAddDel(ActionEvent actionEvent) throws IOException{
+        AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/FXML/wypozyczeniaAddDel.fxml"));
+        pane.getChildren().setAll(anchorPane);
+    }
 }
